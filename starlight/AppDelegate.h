1afef684e999ec531e44985e3e8c48bdb3b768fe
SmartWork <smartwork@smartworks-mac.local>
2018-05-23 04:01:41 -0700

    Initial Commit

---
+++ starlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  starlight
+//
+//  Created by SmartWork on 5/23/18.
+//  Copyright © 2018 OldMayfallia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ starlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  starlight
+//
+//  Created by SmartWork on 5/23/18.
+//  Copyright © 2018 OldMayfallia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
